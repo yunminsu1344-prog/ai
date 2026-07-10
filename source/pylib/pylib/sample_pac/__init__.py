c3be31c974ccfde29839018d87ff932f9a8f0ae3
yunminsu1344@gmail.com <yunminsu1344@gmail.com>
2026-07-10 18:02:24 +0900

    numpy배열생성및복사

---
+++ source/pylib/pylib/sample_pac/__init__.py
@@ -0,0 +1,7 @@
+# sample_pac/__init__.py파일
+# sample_pac 패키지를 import 할 때 자동적으로 메모리에 로드되는 파일
+
+print("sample_pac 패키지를 로드 확인(__init__.py)")
+
+def test():
+    print('test해 봄')
---
+++ source/pylib/pylib/sample_pac/ab/a.py
@@ -0,0 +1,8 @@
+# sample_pac/ab/a.py
+# 실행 :pylib\sample_pac\ab> python a.py
+#       pylib>python -m sample_pac.ab.a
+def hello():
+    print('sample_pac/ab/a모듈의 hello')
+
+if __name__=="__main__":
+    hello()
