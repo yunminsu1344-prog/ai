fb5e23da6ad3effafb8ccc6e2f97c404b4989098
yunminsu1344@gmail.com <yunminsu1344@gmail.com>
2026-07-15 18:03:10 +0900

    데이터프레임추출

--- source/01_python/ch05.py
+++ source/01_python/ch05.py
@@ -1,5 +1,5 @@
-1 # ch05. py
-def my_hello(cnt:int): #python를 cnt번 출력
+# ch05.py
+def my_hello(cnt:int): # python을 cnt번 출력+__name__까지 출력
     for i in range(cnt):
         print('Hello, Python', end='\t')
         print('Hi, Python')
