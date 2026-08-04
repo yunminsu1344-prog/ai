bb200dd00336fb23b447174eccccc191ea7a9088
yunminsu1344@gmail.com <yunminsu1344@gmail.com>
2026-08-04 18:01:53 +0900

    자바스크립트

---
+++ source/03_javaScript/ch01/2.js
@@ -0,0 +1,9 @@
+/* 변수 선언시 var(전역변수), let(지역변수), const(상수) */
+let sum = 0;
+// for(초기화 ; 조건 ; 증감){ 반복문들 }
+for (var i=1 ; i<=5 ; i++){
+    sum+=i; //sum = sum + i;
+    console.log('i=', i, '일때까지 누적된 합은', sum);
+}
+console.log('for문 끝')
+console.log('for문 끝난 후 i값은 '+i);
