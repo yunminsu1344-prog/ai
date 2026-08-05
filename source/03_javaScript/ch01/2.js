1e970124d348f984c0e497cfb1681c77af09e6c3
yunminsu1344@gmail.com <yunminsu1344@gmail.com>
2026-08-05 18:01:56 +0900

    Js 반복문

--- source/03_javaScript/ch01/2.js
+++ source/03_javaScript/ch01/2.js
@@ -2,8 +2,8 @@
 let sum = 0;
 // for(초기화 ; 조건 ; 증감){ 반복문들 }
 for (var i=1 ; i<=5 ; i++){
-    sum+=i; //sum = sum + i;
-    console.log('i=', i, '일때까지 누적된 합은', sum);
+  sum += i; //sum = sum + i;
+  console.log('i=', i, '일때까지 누적된 합은', sum);
 }
 console.log('for문 끝')
 console.log('for문 끝난 후 i값은 '+i);
